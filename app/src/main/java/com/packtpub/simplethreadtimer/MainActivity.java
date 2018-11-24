461bb03168d873d56c081fdaeb53f5498a54a774
Teoh Kheng Hong <tkhenghong@gmail.com>
2018-11-25 00:46:56 +0800

    first commit

---
+++ app/src/main/java/com/packtpub/simplethreadtimer/MainActivity.java
@@ -0,0 +1,75 @@
+package com.packtpub.simplethreadtimer;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Handler myHandler;
+    boolean gameOn;
+    long startTime;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // How many milliseconds is it since the UNIX epoch
+                startTime = System.currentTimeMillis();
+        myHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (gameOn) {
+                    long seconds = ((System.currentTimeMillis() -
+                            startTime)) / 1000;
+                    Log.i("info", "seconds = " + seconds);
+                }
+                myHandler.sendEmptyMessageDelayed(0, 1000);
+            }
+        };
+
+        gameOn = true;
+        myHandler.sendEmptyMessage(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
